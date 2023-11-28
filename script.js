3089bd9e5f7f567fbf74ed5b9e74998740aeef82
kylechi05 <kylechi2157@gmail.com>
2023-11-28 12:05:12 -0600

    lots done

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+// Slide Movement
 const scrollButton = document.getElementsByClassName("material-symbols-outlined");
 const slides = document.getElementsByClassName("slides");
-
-
 scrollButton[0].addEventListener("click", up);
 scrollButton[1].addEventListener("click", down);
 
-
 let clicks = 0;
 slides[clicks].scrollIntoView();
+
+function up() {
+    if (clicks > 0) {
+        clicks--;
+        console.log("Scrolled to " + clicks);
+        slides[clicks].scrollIntoView();
+    }
+}
 
 function down() {
     if (clicks < slides.length - 1) {
@@ -17,10 +23,21 @@ function down() {
     }
 }
 
-function up() {
-    if (clicks > 0) {
-        clicks--;
-        console.log("Scrolled to " + clicks);
-        slides[clicks].scrollIntoView();
+
+
+// Image Iterator for each slide
+var i = 0;
+
+// Shows Image on Click
+function advanceOne(imageArray) {
+    console.log('hei');
+    if (i < imageArray.length){
+        imageArray[i].style.display = 'block';
+        i++;
     }
 }
+
+// Intro Slide Images
+const intro = document.getElementById("intro");
+const introImages = document.getElementsByClassName("introImages");
+intro.addEventListener("click", () => advanceOne(introImages));
